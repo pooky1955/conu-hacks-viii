d4c05f88f296e0ff778f7dba7bdca8ffb1986ec8
Ben <bensaine09@gmail.com>
2024-01-21 10:16:10 -0500

    BIG FIX IS THE NAME OF THIS COMMIT

--- src/LiveGesture.svelte
+++ src/LiveGesture.svelte
@@ -1,176 +1,161 @@
 <script>
-     import { GestureRecognizer, FilesetResolver, DrawingUtils } from "@mediapipe/tasks-vision";
-//   let vision, gestureRecognizer;
-  (async function (){
+	import { GestureRecognizer, FilesetResolver, DrawingUtils } from "@mediapipe/tasks-vision"
+	//   let vision, gestureRecognizer;
+	;(async function () {
+		let vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm")
+		let gestureRecognizer = await GestureRecognizer.createFromModelPath(
+			vision,
+			"https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task"
+		)
 
- let vision = await FilesetResolver.forVisionTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
-);
- let gestureRecognizer = await GestureRecognizer.createFromModelPath(vision,
-    "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task"
-);
+		// const image = document.getElementById("image");
+		// const recognitions = gestureRecognizer.recognize(image);
 
-// const image = document.getElementById("image");
-// const recognitions = gestureRecognizer.recognize(image);
+		let webcamRunning = false
+		const videoHeight = "360px"
+		const videoWidth = "480px"
 
-const demosSection = document.getElementById("demos");
-let runningMode = "IMAGE";
-let enableWebcamButton;
-let webcamRunning = false;
-const videoHeight = "360px";
-const videoWidth = "480px";
+		navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then((myStream) => {
+			video.srcObject = myStream
+			video.addEventListener("loadeddata", predictWebcam)
+			webcamRunning = true
+		})
 
-// Before we can use HandLandmarker class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
-const createGestureRecognizer = async () => {
-  const vision = await FilesetResolver.forVisionTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
-  );
-  gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
-    baseOptions: {
-      modelAssetPath:
-        "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
-      delegate: "GPU"
-    },
-    runningMode: runningMode
-  });
-  demosSection.classList.remove("invisible");
-};
-createGestureRecognizer();
+		// Before we can use HandLandmarker class we must wait for it to finish
+		// loading. Machine Learning models can be large and take a moment to
+		// get everything needed to run.
+		const createGestureRecognizer = async () => {
+			const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm")
+			gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+				baseOptions: {
+					modelAssetPath: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+					delegate: "GPU",
+				},
+				runningMode: "VIDEO",
+			})
+		}
+		createGestureRecognizer()
 
-const video = document.getElementById("webcam");
-const canvasElement = document.getElementById("output_canvas");
-const canvasCtx = canvasElement.getContext("2d");
-const gestureOutput = document.getElementById("gesture_output");
+		const video = document.getElementById("webcam")
+		const canvasElement = document.getElementById("output_canvas")
+		const canvasCtx = canvasElement.getContext("2d")
+		const gestureOutput = document.getElementById("gesture_output")
 
-// Check if webcam access is supported.
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-}
+		// Check if webcam access is supported.
+		// function hasGetUserMedia() {
+		// 	return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
+		// }
 
-// If webcam supported, add event listener to button for when user
-// wants to activate it.
-if (hasGetUserMedia()) {
-  enableWebcamButton = document.getElementById("webcamButton");
-  enableWebcamButton.addEventListener("click", enableCam);
-} else {
-  console.warn("getUserMedia() is not supported by your browser");
-}
+		// If webcam supported, add event listener to button for when user
+		// wants to activate it.
+		// if (hasGetUserMedia()) {
+		// 	enableWebcamButton = document.getElementById("webcamButton")
+		// 	enableWebcamButton.addEventListener("click", enableCam)
+		// } else {
+		// 	console.warn("getUserMedia() is not supported by your browser")
+		// }
 
-// Enable the live webcam view and start detection.
-function enableCam(event) {
-  if (!gestureRecognizer) {
-    alert("Please wait for gestureRecognizer to load");
-    return;
-  }
+		// Enable the live webcam view and start detection.
+		// function enableCam(event) {
+		// 	if (!gestureRecognizer) {
+		// 		alert("Please wait for gestureRecognizer to load")
+		// 		return
+		// 	}
 
-  if (webcamRunning === true) {
-    webcamRunning = false;
-    enableWebcamButton.innerText = "ENABLE PREDICTIONS";
-  } else {
-    webcamRunning = true;
-    enableWebcamButton.innerText = "DISABLE PREDICTIONS";
-  }
+		// 	if (webcamRunning === true) {
+		// 		webcamRunning = false
+		// 		enableWebcamButton.innerText = "ENABLE PREDICTIONS"
+		// 	} else {
+		// 		webcamRunning = true
+		// 		enableWebcamButton.innerText = "DISABLE PREDICTIONS"
+		// 	}
 
-  // getUsermedia parameters.
-  const constraints = {
-    video: true
-  };
+		// 	// getUsermedia parameters.
+		// 	const constraints = {
+		// 		video: true,
+		// 	}
 
-  // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-    video.srcObject = stream;
-    video.addEventListener("loadeddata", predictWebcam);
-  });
-}
+		// 	// Activate the webcam stream.
+		// 	navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+		// 		video.srcObject = stream
+		// 		video.addEventListener("loadeddata", predictWebcam)
+		// 	})
+		// }
 
-let lastVideoTime = -1;
-let results = undefined;
-async function predictWebcam() {
-  const webcamElement = document.getElementById("webcam");
-  // Now let's start detecting the stream.
-  if (runningMode === "IMAGE") {
-    runningMode = "VIDEO";
-    await gestureRecognizer.setOptions({ runningMode: "VIDEO" });
-  }
-  let nowInMs = Date.now();
-  if (video.currentTime !== lastVideoTime) {
-    lastVideoTime = video.currentTime;
-    results = gestureRecognizer.recognizeForVideo(video, nowInMs);
-  }
+		let lastVideoTime = -1
+		let results = undefined
+		async function predictWebcam() {
+			const webcamElement = document.getElementById("webcam")
+			// Now let's start detecting the stream.
+			let nowInMs = Date.now()
+			if (video.currentTime !== lastVideoTime) {
+				lastVideoTime = video.currentTime
+				results = gestureRecognizer.recognizeForVideo(video, nowInMs)
+			}
 
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  const drawingUtils = new DrawingUtils(canvasCtx);
+			canvasCtx.save()
+			canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height)
+			const drawingUtils = new DrawingUtils(canvasCtx)
 
-  canvasElement.style.height = videoHeight;
-  webcamElement.style.height = videoHeight;
-  canvasElement.style.width = videoWidth;
-  webcamElement.style.width = videoWidth;
+			canvasElement.style.height = videoHeight
+			webcamElement.style.height = videoHeight
+			canvasElement.style.width = videoWidth
+			webcamElement.style.width = videoWidth
 
-  if (results.landmarks) {
-    // // console.log(results.landmarks)
-    for (const landmarks of results.landmarks) {
-      drawingUtils.drawConnectors(
-        landmarks,
-        GestureRecognizer.HAND_CONNECTIONS,
-        {
-          color: "#00FF00",
-          lineWidth: 5
-        }
-      );
-      drawingUtils.drawLandmarks(landmarks, {
-        color: "#FF0000",
-        lineWidth: 2
-      });
-    }
-  }
-  canvasCtx.restore();
-  if (results.gestures.length > 0) {
-    // alert("found you!")
-    // alert(JSON.stringify(results))
-    gestureOutput.style.display = "block";
-    gestureOutput.style.width = videoWidth;
-    const categoryName = results.gestures[0][0].categoryName;
-    const categoryScore = parseFloat(
-      results.gestures[0][0].score * 100
-    ).toFixed(2);
-    const handedness = results.handednesses[0][0].displayName;
-    gestureOutput.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore} %\n Handedness: ${handedness}`;
-    // debugger
-    let average = {x:0,y:0,z:0}
-    for (let landmark of results.landmarks[0]){
-      const {x,y,z} = landmark
-      // alert(landmark)
-      average.x += x
-      average.y += y
-      average.z += z
-    }
-    average.x /= results.landmarks[0].length
-    average.y /= results.landmarks[0].length
-    average.z /= results.landmarks[0].length
-    average.z *= 10
-    average.z += 1
-    window.hand = average
+			if (results.landmarks) {
+				// console.log(results.landmarks)
+				for (const landmarks of results.landmarks) {
+					drawingUtils.drawConnectors(landmarks, GestureRecognizer.HAND_CONNECTIONS, {
+						color: "#00FF00",
+						lineWidth: 5,
+					})
+					drawingUtils.drawLandmarks(landmarks, {
+						color: "#FF0000",
+						lineWidth: 2,
+					})
+				}
+			}
+			canvasCtx.restore()
+			if (results.gestures.length > 0) {
+				// alert("found you!")
+				// alert(JSON.stringify(results))
+				gestureOutput.style.display = "block"
+				gestureOutput.style.width = videoWidth
+				const categoryName = results.gestures[0][0].categoryName
+				const categoryScore = parseFloat(results.gestures[0][0].score * 100).toFixed(2)
+				const handedness = results.handednesses[0][0].displayName
+				gestureOutput.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore} %\n Handedness: ${handedness}`
+				// debugger
+				let average = { x: 0, y: 0, z: 0 }
+				for (let landmark of results.landmarks[0]) {
+					const { x, y, z } = landmark
+					// alert(landmark)
+					average.x += x
+					average.y += y
+					average.z += z
+				}
+				average.x /= results.landmarks[0].length
+				average.y /= results.landmarks[0].length
+				average.z /= results.landmarks[0].length
+				average.z *= 10
+				average.z += 1
+				window.hand = average
 
-    // alert(`average is ${JSON.stringify(average)}`)
-      drawingUtils.drawLandmarks([average], {
-        color: "#0000FF",
-        lineWidth: 2
-      });
-      gestureOutput.innerHTML = `Position: ${JSON.stringify(average)}`
-    
-  } else {
-    gestureOutput.style.display = "none";
-  }
-  // Call this function again to keep predicting when the browser is ready.
-  if (webcamRunning === true) {
-    window.requestAnimationFrame(predictWebcam);
-  }
-}
-  })()
-
+				// alert(`average is ${JSON.stringify(average)}`)
+				drawingUtils.drawLandmarks([average], {
+					color: "#0000FF",
+					lineWidth: 2,
+				})
+				gestureOutput.innerHTML = `Position: ${JSON.stringify(average)}`
+			} else {
+				gestureOutput.style.display = "none"
+			}
+			// Call this function again to keep predicting when the browser is ready.
+			if (webcamRunning === true) {
+				window.requestAnimationFrame(predictWebcam)
+			}
+		}
+	})()
 </script>
 
 <div id="liveView" class="videoView">
